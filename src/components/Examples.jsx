95dfd4870970aaf93eca88a8c2faebba97fff3c8
jeongo <jinism2736@gmail.com>
2024-02-14 17:57:44 +0900

    [refactor]기능에 따른 컴포넌트 분리

---
+++ src/components/Examples.jsx
@@ -0,0 +1,58 @@
+import TabButton from "./TabButton";
+import {useState} from "react";
+import {EXAMPLES} from "../data";
+
+export default function Examples() {
+
+    const [selectedTopic, setSelectedTopic] = useState(null);
+    function handleSelect(selectedButton) {
+        setSelectedTopic(selectedButton);
+        console.log(selectedTopic);
+    }
+
+    let tabContent = <p>Please select a topic.</p> ;
+    if(selectedTopic){
+        tabContent = <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+                    <code>
+                        {EXAMPLES[selectedTopic].code}
+                    </code>
+                </pre>
+        </div>
+    }
+
+    return (
+        <section id='examples'>
+            <h2>Examples</h2>
+            <menu>
+                <TabButton
+                    onSelect={() => handleSelect('components')}
+                    isSelected={ selectedTopic === 'components'}
+                >
+                    Components
+                </TabButton>
+                <TabButton
+                    onSelect={() => handleSelect('jsx')}
+                    isSelected={ selectedTopic === 'jsx'}
+                >
+                    JSX
+                </TabButton>
+                <TabButton
+                    onSelect={() => handleSelect('props')}
+                    isSelected={ selectedTopic === 'props'}
+                >
+                    Props
+                </TabButton>
+                <TabButton
+                    onSelect={() => handleSelect('state')}
+                    isSelected={ selectedTopic === 'state'}
+                >
+                    State
+                </TabButton>
+            </menu>
+            {tabContent}
+        </section>
+    )
+}
